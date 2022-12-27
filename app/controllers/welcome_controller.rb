3547f7e15c3eb24a069e310199cf43e15133625c
shoya15 <s.h.rainight@gmail.com>
2022-12-27 19:22:10 +0900

    完了画面に遷移するようにした

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,9 @@
 class WelcomeController < ApplicationController
-  def home
-  end
+  def home; end
 
-  def about
-  end
+  def about; end
 
-  def contact
-  end
+  def contact; end
+
+  def complete; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/about', to: 'welcome#about'
   get '/contact', to: 'welcome#contact'
+  get '/complete', to: 'welcome#complete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
