a0525b8cdf03e60aa3dfbfca50206c1c43726c81
shoya15 <s.h.rainight@gmail.com>
2022-12-30 18:51:32 +0900

    ダイアログと音声合成が正常に作動するように修正した

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+// turboをコメントアウトしておく、ページ遷移すると発火しなくなるため（高速化のためにあるらしいが今回は不要）
+// import "@hotwired/turbo-rails"
 import "controllers"
+
+import "./index";
