592fd7515d95cbd6017293a054564cc0aba5355f
shoya15 <s.h.rainight@gmail.com>
2023-03-04 03:25:52 +0900

    turbo修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// turboをコメントアウトしておく、ページ遷移すると発火しなくなるため（高速化のためにあるらしいが今回は不要）
-// import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
 import "controllers"
 
-import "./index.js";
+import "./index.js"
