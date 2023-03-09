a8162368db3e1d09cc862171ad90b73f34d01b3e
shoya15 <s.h.rainight@gmail.com>
2023-03-09 12:37:02 +0900

    master.keyの読み込み

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, "/var/www/informer"
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
