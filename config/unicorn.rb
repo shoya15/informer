e54e30145b429458d5713d737a062d2cda26d244
shoya15 <s.h.rainight@gmail.com>
2023-03-11 10:22:54 +0900

    unicornのtimeout変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes  1
 working_directory "#{app_path}/current"
-timeout           15
+timeout           70
 working_directory app_path
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
