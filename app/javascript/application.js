b4fddb17fa0becfc6ee9b0dd51392ece00a22602
shoya15 <s.h.rainight@gmail.com>
2023-03-12 17:19:29 +0900

    import @hotwired/turbo-rails errorの解消

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+// import "@hotwired/turbo-rails"
 import "controllers"
 
 import "./index-9446d18a50bcfb512a368cb9f0e69423c0393fc45e889b41be1cd63a0ac62b46.js"
